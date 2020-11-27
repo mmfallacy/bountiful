29bbe1937f2b8c175b00e5c3ec9c0aeef54e3367
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2020-11-27 19:07:08 +0800

    FIX: Removed typescript specific code and made all tsx to jsx

--- src/components/PrimaryButton/PrimaryButton.jsx
+++ src/components/PrimaryButton/PrimaryButton.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import Style from './PrimaryButton.module.scss'
 
-interface PrimaryButtonProps{
-    label: string,
 
-}
-
-export default function PrimaryButton({label}: PrimaryButtonProps){
+export default function PrimaryButton({label}){
     return (
         <button className={Style.PrimaryButton}>
             <h3 className={Style.Label}>{label}</h3>
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -3,7 +3,7 @@ import Header from '../../components/Header/Header'
 import Style from './Main.module.scss'
 
 function Main(): JSX.Element{
-    const [isInSellerMode, setMode] = useState<boolean>(false)
+    const [isInSellerMode, setMode] = useState(false)
     return (
         <div>
             <Header />
