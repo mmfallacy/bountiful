33897e31dedb395e50b0cc616879c03ea36da48f
Garth Hendrich Lapitan <garthlapitan.up@gmail.com>
2020-11-27 21:58:33 +0800

    feat: add offer card

--- src/components/Card/OfferCard.jsx
+++ src/components/Card/OfferCard.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { ReactComponent as ArrowOpen } from "./ArrowOpen.svg";
 import Style from "./Card.module.scss";
 
-export default function RequestCard({ imageSrc, name, price }) {
+export default function OfferCard({ imageSrc, seller, desc, price, name }) {
   return (
     <div className={Style.ListCard}>
       <img className={Style.Image} src={imageSrc} alt={name} />
       <div className={Style.Info}>
-        <h3 className={Style.Label}>{name}</h3>
-        <p className={Style.Price}>Suggested Price: Php {price}</p>
+        <h3 className={Style.Label}>{seller}</h3>
+        <p className={Style.Desc}>{desc}</p>
+        <p className={Style.Price}>Price: Php {price}</p>
       </div>
       <ArrowOpen className={Style.ArrowOpen} />
     </div>
