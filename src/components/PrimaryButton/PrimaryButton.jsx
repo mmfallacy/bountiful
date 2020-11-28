2c26e45cdcac9e0858abb84bbb906b92a4ac468a
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2020-11-28 17:05:15 +0800

    UI: Refactor Primary Button styling

--- src/components/PrimaryButton/PrimaryButton.jsx
+++ src/components/PrimaryButton/PrimaryButton.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Style from './PrimaryButton.module.scss'
 
 
-export default function PrimaryButton({label}){
+export default function PrimaryButton({label, variant="h3"}){
+    const Variant = variant
     return (
         <button className={Style.PrimaryButton}>
-            <h3 className={Style.Label}>{label}</h3>
+            <Variant className={Style.Label}>{label}</Variant>
         </button>
     )
 }
