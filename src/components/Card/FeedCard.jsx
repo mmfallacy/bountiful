7d21ad9a405ee88422928d5f371d1b99d8ada058
Garth Hendrich Lapitan <garthlapitan.up@gmail.com>
2020-11-28 14:23:24 +0800

    feat: update cards

--- src/components/Card/FeedCard.jsx
+++ src/components/Card/FeedCard.jsx
@@ -1,20 +1,18 @@
 import React from "react";
-import { ReactComponent as ArrowOpen } from "./ArrowOpen.svg";
+import { ReactComponent as OfferSvg } from "./Offer.svg";
 import Style from "./Card.module.scss";
 
-export default function FeedCard({ name, price, desc, imgSrc }) {
+export default function FeedCard({ product, budget, imgSrc }) {
   return (
-    <div className={`${Style.FeedCard} ${Style.Card}`}>
-      <h3 className={Style.Label}>{name}</h3>
-      <p className={Style.Price}>Suggested Price: Php {price}</p>
-      <p className={Style.Desc}>{desc}</p>
-      <img className={Style.Image} src={imgSrc} alt={name} />
+    <div className={`${Style.Card} ${Style.FeedCard}`}>
+      {imgSrc && <img className={Style.Image} src={imgSrc} alt={product} />}
       <div className={Style.Footer}>
-        {/* <div className={Style.FooterContent}> */}
-        <p>Make an offer</p>
-        <ArrowOpen className={Style.ArrowOpen} />
+        <div className={Style.Info}>
+          <h3 className={Style.Label}>{product}</h3>
+          <p className={Style.Price}>Budget: Php {budget}</p>
+        </div>
+        <OfferSvg className={Style.Offer} />
       </div>
-      {/* </div> */}
     </div>
   );
 }
