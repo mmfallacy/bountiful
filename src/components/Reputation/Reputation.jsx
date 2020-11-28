52099f9f9ed4b46decb1f0d0f442388a7eb31af1
Garth Hendrich Lapitan <garthlapitan.up@gmail.com>
2020-11-28 21:46:52 +0800

    fix: wrong condition for half-medal in Reputation
    * change medal svgs
    * add new-medal svg

--- src/components/Reputation/Reputation.jsx
+++ src/components/Reputation/Reputation.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { ReactComponent as MedalSvg } from "./Medal.svg";
+import { ReactComponent as FullMedalSvg } from "./FullMedal.svg";
 import { ReactComponent as HalfMedalSvg } from "./HalfMedal.svg";
+import { ReactComponent as NewMedalSvg } from "./NewMedal.svg";
 import Style from "./Reputation.module.scss";
 
 export default function Reputation({ score }) {
   const medals = [];
-  for (let i = 0; i < Math.floor(score); i++) medals.push(<MedalSvg />);
-  if (score % 2 === 0.5) medals.push(<HalfMedalSvg />);
+  for (let i = 0; i < Math.floor(score); i++) medals.push(<FullMedalSvg />);
+  if (score % 1 === 0.5) medals.push(<HalfMedalSvg />);
+  for (let i = 0; i < Math.floor(5 - score); i++) medals.push(<NewMedalSvg />);
 
   return <div className={Style.Reputation}>{medals}</div>;
 }
