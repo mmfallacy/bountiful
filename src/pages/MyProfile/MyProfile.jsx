a133b08cd0eb766b698ae888108308f6bf54f538
Garth Hendrich Lapitan <garthlapitan.up@gmail.com>
2020-11-28 20:40:45 +0800

    feat: add MyProfile component

---
+++ src/pages/MyProfile/MyProfile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ReactComponent as EditSvg } from "./EditSvg.svg";
+import PageHeader from "../../components/PageHeader/PageHeader";
+import Style from "./MyProfile.module.scss";
+
+export default function MyProfile({ pfp, name, email, bio }) {
+  return (
+    <div>
+      <PageHeader label="My account" />
+      <div className={Style.BodyBox}>
+        <div className={Style.AccountInfo}>
+          <img className={Style.Pfp} src={pfp} alt={name} />
+          <h1>{name}</h1>
+          <p>{email}</p>
+          <p>{bio}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
