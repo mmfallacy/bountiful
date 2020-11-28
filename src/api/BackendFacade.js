a512b061cffd3d4802df0d67078111e31fc70f84
Basti Ortiz <ortizbasti16@gmail.com>
2020-11-29 00:13:03 +0800

    Feat: include the array of listings for each user

--- src/api/BackendFacade.js
+++ src/api/BackendFacade.js
@@ -9,6 +9,7 @@ import 'firebase/firestore';
  * @property {string} email - Google email of the user
  * @property {string} bio - Bio description of the user
  * @property {number} karma - Rating of the user from 0.0 to 1.0
+ * @property {string[]} listings - Array of listing IDs posted by the user
  * @property {string} pfp - URL to the profile picture
  */
 
@@ -16,6 +17,7 @@ import 'firebase/firestore';
  * @typedef {Object} Listing
  * @property {string} id
  * @property {string} author - user ID of the person who posted the listing
+ * @property {number} price
  * @property {string} title
  * @property {string} description
  * @property {string} photo
@@ -43,27 +45,37 @@ export class BackendFacade {
     const provider = new firebase.auth.GoogleAuthProvider();
     const auth = this._app.auth();
     await auth.signInWithRedirect(provider);
-    const user = auth.currentUser;
-    this._user = {
-      uid: user.uid,
-      name: user.displayName,
-      email: user.email,
-      // TODO: Implement proper way of editing bio
-      bio: "Lorem ipsum.",
-      // TODO: Implement proper rating system
-      karma: Math.random(),
-      pfp: user.photoURL,
-    };
+
+    // Extract user information
+      const {
+          uid,
+          displayName: name,
+          email,
+          photoURL: pfp
+      } = auth.currentUser;
 
     // Check if the user already exists
     const db = this._app.firestore();
     const collection = db.collection('users');
-    const snapshot = await collection.where('uid', '==', user.uid).get();
-    if (snapshot.empty)
+    const snapshot = await collection.where('uid', '==', uid).get();
+    if (snapshot.empty) {
+      this._user = {
+          uid,
+          name,
+          email,
+          // TODO: Implement proper way of editing bio
+          bio: "Lorem ipsum",
+          // TODO: Implement proper rating system
+          karma: Math.random(),
+          listings: [],
+          pfp,
+      };
       await collection.add(this._user);
+    } else
+      // @ts-expect-error
+      this._user = snapshot.docs[0].data();
 
     return this._user;
-
   }
 
   /** @returns {Promise<void>} */
@@ -73,24 +85,24 @@ export class BackendFacade {
   }
 
   /** @returns {Promise<Listing[]>} */
-  async retrieveListings() {
+  async retrieveAllListings() {
     const db = this._app.firestore();
     const snapshot = await db.collection('listings').get();
     /** @type {Listing[]} */
     // @ts-expect-error
-    const docs = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-    return docs;
+    return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
   }
 
   /**
-   * @param {string} id
-   * @returns {Promise<Listing>}
+   * @param {string[]} ids
+   * @returns {Promise<Listing[]>}
    */
-  async retrieveListingById(id) {
+  async retrieveMultipleListingsById(ids) {
     const db = this._app.firestore();
-    const snapshot = await db.collection('listings').doc(id).get();
+    const DOC_ID = firebase.firestore.FieldPath.documentId();
+    const snapshots = await db.collection('listings').where(DOC_ID, 'in', ids).get();
     // @ts-expect-error
-    return { id: snapshot.id, ...snapshot.data() };
+    return snapshots.docs.map(doc => ({ id: doc.id, ...doc.data() }));
   }
 
   /**
@@ -111,28 +123,36 @@ export class BackendFacade {
   /**
    * @param {string} title
    * @param {string} description
+   * @param {number} price
    * @param {string} photo - Data URL of the photo uploaded
    * @returns {Promise<Listing>}
    */
-  async createListing(title, description, photo) {
+  async createListing(title, description, price, photo) {
     const user = this.user;
     if (!user)
       throw new Error('There is no user logged in!');
 
+    // Add listing to the database
     const db = this._app.firestore();
     /** @type {Omit<Listing, 'id'>} */
     const listing = {
         author: user.uid,
         title,
         description,
+        price,
         photo,
     };
-    const reference = await db.collection('listings').add(listing);
-    const doc = await reference.get();
+    const listingSnapshot = await db.collection('listings').add(listing);
+    const doc = await listingSnapshot.get();
+
+    // Add reference of the new listing to the current user
+    const userSnapshot = await db.collection('users').where('uid', '==', this._user.uid).get();
+    this._user.listings.push(listingSnapshot.id);
+    await userSnapshot.docs[0].ref.update('listings', this._user.listings);
 
     // @ts-expect-error
     return {
-      id: reference.id,
+      id: listingSnapshot.id,
       ...doc.data(),
     }
   }
