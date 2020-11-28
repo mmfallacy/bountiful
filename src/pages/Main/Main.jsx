97be53dab4987e5dd35330b39072826e73612219
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2020-11-29 06:56:38 +0800

    CHORE: Refactor mock data placement

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,80 +1,83 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {BackgroundBlobRepeat, FeedCard, SearchBar, Navbar} from "../../components"
 
 import Style from "./Main.module.scss";
 
+const mockData = [
+  {
+    productName: "Black lamb",
+    requestId: "IDutyqwretyuretuyer",
+    budget: 100,
+    price: 150,
+    description:
+      "What was beyond the bend in the stream was unknown. Both were curious, but only one was brave enough to want to explore. That was the problem. There was always one that let fear rule her life.",
+    productImage: "https://via.placeholder.com/150",
+    sellerId: "IDovsdainfedsmdfs6969",
+  },
+  {
+    productName: "Black lamb",
+    requestId: "IDfpaowejpweo",
+    budget: 100,
+    price: 120,
+    description:
+      "I recollect that my first exploit in squirrel-shooting was in a grove of tall walnut-trees that shades one side of the valley. I had wandered into it at noontime, when all nature is peculiarly quiet, and was startled by the roar of my own gun, as it broke the Sabbath stillness around and was prolonged and reverberated by the angry echoes.",
+    productImage: "https://via.placeholder.com/200",
+    sellerId: "IDgdsfgsdfgdsfsdf",
+  },
+  {
+    productName: "Black lamb",
+    requestId: "IDkfnkvfdkfdkdf",
+    budget: 100,
+    price: 130,
+    description:
+      "She considered the birds to be her friends. She'd put out food for them each morning and then she'd watch as they came to the feeders to gorge themselves for the day. She wondered what they would do if something ever happened to her. Would they miss the meals she provided if she failed to put out the food one morning?",
+    productImage: "https://via.placeholder.com/330x150",
+    sellerId: "IDknbvnkvnnvbkb",
+  },
+  {
+    productName: "Black lamb",
+    requestId: "IDkfnkvfdkfdkdf",
+    budget: 100,
+    price: 130,
+    description:
+      "She considered the birds to be her friends. She'd put out food for them each morning and then she'd watch as they came to the feeders to gorge themselves for the day. She wondered what they would do if something ever happened to her. Would they miss the meals she provided if she failed to put out the food one morning?",
+    productImage: "https://via.placeholder.com/330x150",
+    sellerId: "IDa",
+  },
+  {
+    productName: "Black lamb",
+    requestId: "IDkfnkvfdkfdkdf",
+    budget: 100,
+    price: 130,
+    description:
+      "She considered the birds to be her friends. She'd put out food for them each morning and then she'd watch as they came to the feeders to gorge themselves for the day. She wondered what they would do if something ever happened to her. Would they miss the meals she provided if she failed to put out the food one morning?",
+    productImage: "https://via.placeholder.com/330x150",
+    sellerId: "IDab",
+  },
+  {
+    productName: "Black lamb",
+    requestId: "IDkfnkvfdkfdkdf",
+    budget: 100,
+    price: 130,
+    description:
+      "She considered the birds to be her friends. She'd put out food for them each morning and then she'd watch as they came to the feeders to gorge themselves for the day. She wondered what they would do if something ever happened to her. Would they miss the meals she provided if she failed to put out the food one morning?",
+    productImage: "https://via.placeholder.com/330x150",
+    sellerId: "IDabc",
+  },
+];
+
+const name = "Lance Admin";
+const pfp = "https://via.placeholder.com/100";
+
 function Main() {
-  const [isInSellerMode, setMode] = useState(false);
+  const [listings, setListings] = useState([]);
 
-  const name = "Lance Admin";
-  const pfp = "https://via.placeholder.com/100";
+  useEffect(()=>{
+    setListings(mockData)
+  })
 
-  const mockData = [
-    {
-      productName: "Black lamb",
-      requestId: "IDutyqwretyuretuyer",
-      budget: 100,
-      price: 150,
-      description:
-        "What was beyond the bend in the stream was unknown. Both were curious, but only one was brave enough to want to explore. That was the problem. There was always one that let fear rule her life.",
-      productImage: "https://via.placeholder.com/150",
-      sellerId: "IDovsdainfedsmdfs6969",
-    },
-    {
-      productName: "Black lamb",
-      requestId: "IDfpaowejpweo",
-      budget: 100,
-      price: 120,
-      description:
-        "I recollect that my first exploit in squirrel-shooting was in a grove of tall walnut-trees that shades one side of the valley. I had wandered into it at noontime, when all nature is peculiarly quiet, and was startled by the roar of my own gun, as it broke the Sabbath stillness around and was prolonged and reverberated by the angry echoes.",
-      productImage: "https://via.placeholder.com/200",
-      sellerId: "IDgdsfgsdfgdsfsdf",
-    },
-    {
-      productName: "Black lamb",
-      requestId: "IDkfnkvfdkfdkdf",
-      budget: 100,
-      price: 130,
-      description:
-        "She considered the birds to be her friends. She'd put out food for them each morning and then she'd watch as they came to the feeders to gorge themselves for the day. She wondered what they would do if something ever happened to her. Would they miss the meals she provided if she failed to put out the food one morning?",
-      productImage: "https://via.placeholder.com/330x150",
-      sellerId: "IDknbvnkvnnvbkb",
-    },
-    {
-      productName: "Black lamb",
-      requestId: "IDkfnkvfdkfdkdf",
-      budget: 100,
-      price: 130,
-      description:
-        "She considered the birds to be her friends. She'd put out food for them each morning and then she'd watch as they came to the feeders to gorge themselves for the day. She wondered what they would do if something ever happened to her. Would they miss the meals she provided if she failed to put out the food one morning?",
-      productImage: "https://via.placeholder.com/330x150",
-      sellerId: "IDa",
-    },
-    {
-      productName: "Black lamb",
-      requestId: "IDkfnkvfdkfdkdf",
-      budget: 100,
-      price: 130,
-      description:
-        "She considered the birds to be her friends. She'd put out food for them each morning and then she'd watch as they came to the feeders to gorge themselves for the day. She wondered what they would do if something ever happened to her. Would they miss the meals she provided if she failed to put out the food one morning?",
-      productImage: "https://via.placeholder.com/330x150",
-      sellerId: "IDab",
-    },
-    {
-      productName: "Black lamb",
-      requestId: "IDkfnkvfdkfdkdf",
-      budget: 100,
-      price: 130,
-      description:
-        "She considered the birds to be her friends. She'd put out food for them each morning and then she'd watch as they came to the feeders to gorge themselves for the day. She wondered what they would do if something ever happened to her. Would they miss the meals she provided if she failed to put out the food one morning?",
-      productImage: "https://via.placeholder.com/330x150",
-      sellerId: "IDabc",
-    },
-  ];
-
-  const feed = [];
-  mockData.map(({ sellerId, productImage, productName, budget }) =>
-    feed.push(<FeedCard key={sellerId} imgSrc={productImage} budget={budget} product={productName} />)
+  const feed = listings.map(({ sellerId, productImage, productName, budget }) =>
+    <FeedCard key={sellerId} imgSrc={productImage} budget={budget} product={productName} />
   );
 
   return (
@@ -83,11 +86,16 @@ function Main() {
         <img className={Style.Pfp} src={pfp} alt={name} />
       </div>
       <div className={Style.Content}>
+
         <h1 className={Style.Greetings}>Hey {name.split(" ")[0]},</h1>
         <h3 className={Style.Catchphrase}>Look for potential buyers now!</h3>
+
         <SearchBar />
+
         <h2 className={Style.Label}>What's New?</h2>
+
         {feed}
+
       </div>
       <Navbar/>
       <BackgroundBlobRepeat/>
