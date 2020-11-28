542370bd5b5ec14646500a793df4eb6b9886beb8
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2020-11-28 17:52:09 +0800

    FEAT: Added basic logic for Form Stepping

--- src/components/MultiStepForm/MultiStepForm.jsx
+++ src/components/MultiStepForm/MultiStepForm.jsx
@@ -1,30 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Style from './MultiStepForm.module.scss'
 import FormStepper from './FormStepper/FormStepper'
 import PrimaryButton from '../PrimaryButton/PrimaryButton'
 
-export function MultiStepForm({className, children}) {
+export function MultiStepForm({className, children, onSubmit}) {
     const formContent = React.Children.toArray(children)
 
-    const isValidChild = (element) => React.isValidElement(element) && ( element.type.prototype instanceof Step )  
+    const isValidChild = (element) => React.isValidElement(element) && element.type.name === "Step"
 
-    const onClickHandler = (e)=>{
+    formContent.forEach(element=>{
+        if(!isValidChild(element))
+            throw Error(`Invalid Child of MultiStepForm: ${element.type.name} must be an instance of Step`)  
+    })
+
+    const onSubmitHandler = (e)=>{
         e.preventDefault()
-        
+        if(activeStep > steps-1){
+            onSubmit(e)
+            setActiveStep(1)
+        }
+        else
+            setActiveStep(step=>step+1)
     }
+
+
+    const [activeStep, setActiveStep] = useState(1) 
+    const steps = formContent.length
 
     return (
         <form className={`${Style.MultiStepForm} ${className}`}
-            onClick={onClickHandler}
+            onSubmit={onSubmitHandler}
         >
             <FormStepper className={Style.FormStepper}
-                steps={3}
-                activeStep={1}
+                {...{steps,activeStep}}
             />
             <div className={Style.FormStepContainer}>
-                {formContent.map((element, idx)=>
-                    isValidChild && React.cloneElement(element, {props:{key: idx}})
-                )}
+                {React.cloneElement(formContent[activeStep-1])}
             </div>
             <Stepper />
         </form>
